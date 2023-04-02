ee8cf1cb6349f9e2189a9b67d5347cdec579357f
abdulrahman112001 <abdulrahmanelshikh2001@gmail.com>
2023-04-02 22:48:12 +0200

    push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,12 @@
 /////////// IMPORTS
 ///
-import { Settings } from "../setting";
-import { Link } from "react-router-dom";
-import { AiFillYoutube } from "react-icons/ai";
-import { FaInstagram } from "react-icons/fa";
-import { FiSearch } from "react-icons/fi";
-import { BsLinkedin, BsFacebook, BsTwitter } from "react-icons/bs"; /////////// Types
-import Nav from "react-bootstrap/Nav";
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import { useState } from "react";
+import { Settings } from "../setting"
+import { Link } from "react-router-dom"
+import { AiFillYoutube } from "react-icons/ai"
+import { FaInstagram } from "react-icons/fa"
+import { FiSearch } from "react-icons/fi"
+import { BsLinkedin, BsFacebook, BsTwitter } from "react-icons/bs" /////////// Types
+import { useState } from "react"
 import logo from "../assets/logo.png"
 ///
 
@@ -21,11 +14,41 @@ import logo from "../assets/logo.png"
 ///
 ///
 const NaveBarLinks = [
-  { id: crypto.randomUUID(), title: "المباريات", Link: "matches" },
-  { id: crypto.randomUUID(), title: "الاخبار", Link: "posts-home" },
-  { id: crypto.randomUUID(), title: "الدوريات والبطولات", Link: "leagues" },
-  { id: crypto.randomUUID(), title: "ميديا المرمى", Link: "medias" },
-  { id: crypto.randomUUID(), title: " اخر الانتقالات", Link: "" },
+  {
+    id: crypto.randomUUID(),
+    title: "المباريات",
+    Link: "matches",
+    subMenu: [
+      { title: "المباريات", Link: "matches" },
+      { title: "المباريات", Link: "matches" },
+      { title: "المباريات", Link: "matches" },
+      { title: "المباريات", Link: "matches" },
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "الاخبار",
+    Link: "posts-home",
+    subMenu: [{ title: "الاخبار", Link: "matches" }],
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "الدوريات والبطولات",
+    Link: "leagues",
+    subMenu: [{ title: "الدوريات", Link: "matches" }],
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "ميديا المرمى",
+    Link: "medias",
+    subMenu: [{ title: "ميديا", Link: "matches" }],
+  },
+  {
+    id: crypto.randomUUID(),
+    title: " اخر الانتقالات",
+    Link: "",
+    subMenu: [{ title: "الانتقالات", Link: "matches" }],
+  },
 ]
 
 ///
@@ -37,9 +60,9 @@ export const NavbarComp = () => {
   /////////// CUSTOM HOOKS
   ///
 
-  const [isNavCollapsed, setIsNavCollapsed] = useState(false);
+  const [isNavCollapsed, setIsNavCollapsed] = useState(false)
 
-  const handleNavCollapse = () => setIsNavCollapsed(true);;
+  const handleNavCollapse = () => setIsNavCollapsed(true)
 
   ///
   /////////// STATES
@@ -90,7 +113,7 @@ export const NavbarComp = () => {
             </div>
           </div>
           {/* ////////////////////////////////////////////////////////////////// */}
-          <nav className="navbar navbar-expand-lg  ">
+          <nav className="navbar navbar-expand-lg w-100 ">
             <div className="container-fluid ">
               <div>
                 <button
@@ -108,7 +131,7 @@ export const NavbarComp = () => {
               </div>
 
               <div
-                className="offcanvas offcanvas-start align-items-center"
+                className="offcanvas offcanvas-start align-items-center h-100"
                 data-bs-scroll="true"
                 id="offcanvasWithBothOptions"
                 aria-labelledby="offcanvasWithBothOptionsLabel"
@@ -132,62 +155,93 @@ export const NavbarComp = () => {
                   ></button>
                 </div>
 
-                <div class="offcanvas-body">
+                <div class="offcanvas-body h-100">
                   <ul className="d-flex justify-content-between m-0 flex-wrap">
-                    <div className="navbar-nav me-auto mb-2 mb-lg-0 m-auto align-items-center">
+                    <div className="navbar-nav me-auto mb-2 mb-lg-0 m-auto gap-2 align-items-center">
                       {NaveBarLinks.map((navLink) => (
-                        <li
-                          key={navLink.id}
-                          className="nav-item mx-4"
-                          data-bs-dismiss="offcanvas"
-                          aria-label="Close"
-                        >
-                          <Link
-                            to={navLink.Link}
-                            className="nav-link active text-white"
-                            aria-current="page"
-                          >
-                            {navLink.title}
-                          </Link>
-                        </li>
+                        <>
+                          {navLink.subMenu ? (
+                            <div className=" dropdown-center  w-100 rounded-2">
+                              <button
+                                type="button"
+                                className="btn nav-ite p-2 bg-transparent border-0  text-white dropdown-toggle "
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                              >
+                                {navLink.title}
+                              </button>
+                              <ul className="dropdown-menu p-0 ulSubMenu">
+                                {navLink.subMenu.map((sub, i) => (
+                                  <li
+                                    key={i}
+                                    className="  bg-white m-0 p-1 subMenu dropdown-item"
+                                    type="button"
+                                    data-bs-dismiss="offcanvas"
+                                    aria-label="Close"
+                                  >
+                                    <Link
+                                      to={sub.Link}
+                                      className=" active"
+                                      aria-current="page"
+                                    >
+                                      {sub.title}
+                                    </Link>
+                                  </li>
+                                ))}
+                              </ul>
+                            </div>
+                          ) : (
+                            <li
+                              key={navLink.id}
+                              className="nav-item mx-4"
+                              data-bs-dismiss="offcanvas"
+                              aria-label="Close"
+                            >
+                              <Link
+                                to={navLink.Link}
+                                className="nav-link active text-white"
+                                aria-current="page"
+                              >
+                                {navLink.title}
+                              </Link>
+                            </li>
+                          )}
+                        </>
                       ))}
-                      <li>
+                    </div>
+                    <div className=" d-flex flex-md-row flex-column gap-3 Social align-items-center justify-content-end p-2 ">
+                      <li className=" mx-4 w-100 bg-transparent">
                         <button
                           type="button"
                           className="btn text-primary position-relative  d-flex align-items-end justify-content-end p-2 w-100"
                         >
-                          <Link to="" className="">
+                          <Link className="w-100" to="">
                             متجر المرمي
                           </Link>
                           <span className="position-absolute top-0 start-75 translate-middle badge rounded-pill bg-danger">
                             new
-                            <span className="visually-hidden">
-                              unread messages
-                            </span>
                           </span>
                         </button>
                       </li>
+                      <div className=" d-flex gap-3 Social align-items-center justify-content-end p-2 ">
+                        <Settings />
+                        <Link to="#" className="">
+                          <AiFillYoutube />
+                        </Link>
+                        <Link to="#" className="">
+                          <BsLinkedin />
+                        </Link>
+                        <Link to="#" className="">
+                          <FaInstagram />
+                        </Link>
+                        <Link to="#" className="">
+                          <BsFacebook />
+                        </Link>
+                        <Link to="#" className="">
+                          <BsTwitter />
+                        </Link>
+                      </div>
                     </div>
-                    <div className=" d-flex gap-3 Social align-items-start justify-content-end p-2 ">
-                      {" "}
-                      <Settings />
-                      <Link to="#" className="">
-                        <AiFillYoutube />
-                      </Link>
-                      <Link to="#" className="">
-                        <BsLinkedin />
-                      </Link>
-                      <Link to="#" className="">
-                        <FaInstagram />
-                      </Link>
-                      <Link to="#" className="">
-                        <BsFacebook />
-                      </Link>
-                      <Link to="#" className="">
-                        <BsTwitter />
-                      </Link>
-                    </div>
-                               
                   </ul>
                 </div>
               </div>
@@ -195,6 +249,6 @@ export const NavbarComp = () => {
           </nav>
         </div>
       </div>
-    </>
-  )
-};
+    </>
+  )
+}
