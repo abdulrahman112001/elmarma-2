d9a6216ba1ae84f5986dda30a5d1f051be67396d
abdulrahman112001 <abdulrahmanelshikh2001@gmail.com>
2023-06-01 03:00:03 +0300

    add active in btn and navbar and add function in dates in matches

--- src/components/SwiperComp.jsx
+++ src/components/SwiperComp.jsx
@@ -5,139 +5,138 @@ import { MdKeyboardArrowLeft } from "react-icons/md";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import { A11y, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useIsRTL } from "../hooks/useIsRTL";
+import { apiClient , apiClientEn } from "../utils/axios-util";
+import ButtonGroups from "./ButtonGroups";
+import Spiner from "./Spiner";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { useIsRTL } from "../hooks/useIsRTL";
-import { apiClient } from "../utils/axios-util";
-import ButtonGroups from "./ButtonGroups";
-import Spiner from "./Spiner";
 
 function SwiperComp() {
   const isRTL = useIsRTL();
-  const [formateValue, setFormateValue  ] = useState();
+  const [formateValue, setFormateValue] = useState();
 
-  const { data: MatchAr ,isFetching ,     isLoading,} = useQuery({
+  const {
+    data: MatchAr,
+    isFetching,
+    isLoading,
+  } = useQuery({
     queryKey: [`ALL-MATCHES-SWIPER/${formateValue}`],
     queryFn: async () => {
       const res = await apiClient.get(`all-matches?date=${formateValue}`);
       return res.data.data;
     },
   });
-  const {
-    data: MatchEng,
-
-    
-  } = useQuery({
+  
+  const { data: MatchEng } = useQuery({
     queryKey: [`MatchEng`],
     queryFn: async () => {
-      const res = await apiClient.get(`all-matches-en`);
+      const res = await apiClientEn.get(`fixtures?live=all`);
       return res.data.data;
     },
   });
-;
+  console.log("🚀 ~ file: SwiperComp.jsx:41 ~ SwiperComp ~ MatchEng:", MatchEng)
   const Slider = !isRTL ? MatchEng || [] : MatchAr || [];
+  console.log("🚀 ~ file: SwiperComp.jsx:42 ~ SwiperComp ~ Slider:", Slider)
   return (
     <>
       <div className="mt-4">
         {isRTL && <ButtonGroups setFormateValue={setFormateValue} />}
 
-
- {      isLoading ? (
-        <p className="text-center">
-          <Spiner variant="dark" />
-          <h6 className="mt-2 text-dark"> {`${t("Loading ....")}`} </h6>
-        </p>
-      ) : Slider.length === 0 ? (
-        "يوجد خطأ في عرض المباريات  "
-      ) :
-
-        <Swiper
-          className="bg-dark text-white"
-          modules={[Navigation, A11y]}
-          spaceBetween={10}
-          navigation
-          breakpoints={{
-            640: {
-              slidesPerView: 1,
-            },
-            768: {
-              slidesPerView: 2,
-            },
-            1000: {
-              slidesPerView: 3,
-            },
-            1500: {
-              slidesPerView: 4,
-            },
-          }}
-        >
-
-          {Slider.map((slide) => (
-            <SwiperSlide key={slide.id}>
-              <Link to={`/details-match${slide?.id}`} className="p-0">
-                <div className="d-flex justify-content-between align-items-center position-relative sliderAfter px-5 py-3">
-                  <div className="d-flex  align-items-center ">
-                    <div className="d-flex flex-column align-items-center m-0 text-center  ">
-                      <img
-                        className="first_image"
-                        src={slide?.first_image}
-                        alt=""
-                        width={"30px"}
-                      />
-                      <small>{slide?.first_team}</small>
-                    </div>
-                  </div>
-
-                  <div className="d-flex  align-items-center ">
-                    <div>{slide?.second_result}</div>
-
-                    <div className="d-flex flex-column  justify-content-center align-items-center ">
-                      <h6 className="titel-match text-center">
-                        {slide?.championship_type}
-                      </h6>
-
-                      <div
-                        className="text-center w-100 rounded-pill mx-4"
-                        style={{ backgroundColor: slide?.badgeColor }}
-                      >
-                        {slide?.match_status}
-                      </div>
-
-                      <div
-                        style={{
-                          transform: "translateY(13px)",
-                          color: "#A7A7A7",
-                        }}
-                        className="game_time"
-                      >
-                        {slide?.game_time}
+        {isLoading ? (
+          <p className="text-center">
+            <Spiner variant="dark" />
+            <h6 className="mt-2 text-dark"> {`${t("Loading ....")}`} </h6>
+          </p>
+        ) : Slider.length === 0 ? (
+          "يوجد خطأ في عرض المباريات  "
+        ) : (
+          <Swiper
+            className="bg-dark text-white"
+            modules={[Navigation, A11y]}
+            spaceBetween={10}
+            navigation
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+              },
+              768: {
+                slidesPerView: 2,
+              },
+              1000: {
+                slidesPerView: 3,
+              },
+              1500: {
+                slidesPerView: 4,
+              },
+            }}
+          >
+            {Slider.map((slide) => (
+              <SwiperSlide key={slide.id}>
+                <Link to={`/details-match${slide?.id}`} className="p-0">
+                  <div className="d-flex justify-content-between align-items-center position-relative sliderAfter px-5 py-3">
+                    <div className="d-flex  align-items-center ">
+                      <div className="d-flex flex-column align-items-center m-0 text-center  ">
+                        <img
+                          className="first_image"
+                          src={slide?.first_image}
+                          alt=""
+                          width={"30px"}
+                        />
+                        <small>{slide?.first_team}</small>
                       </div>
                     </div>
 
-                    <div className="m-0  ">{slide?.first_result}</div>
-                  </div>
+                    <div className="d-flex  align-items-center ">
+                      <div>{slide?.second_result}</div>
 
-                  <div className="d-flex  align-items-center">
-                    <div className="d-flex flex-column align-items-center ">
-                      <img
-                        className="second_image"
-                        src={slide?.second_image}
-                        alt=""
-                        width={"30px"}
-                      />
-                      <small>{slide?.second_team}</small>
+                      <div className="d-flex flex-column  justify-content-center align-items-center ">
+                        <h6 className="titel-match text-center">
+                          {slide?.championship_type}
+                        </h6>
+
+                        <div
+                          className="text-center w-100 rounded-pill mx-4"
+                          style={{ backgroundColor: slide?.badgeColor }}
+                        >
+                          {slide?.match_status}
+                        </div>
+
+                        <div
+                          style={{
+                            transform: "translateY(13px)",
+                            color: "#A7A7A7",
+                          }}
+                          className="game_time"
+                        >
+                          {slide?.game_time}
+                        </div>
+                      </div>
+
+                      <div className="m-0  ">{slide?.first_result}</div>
+                    </div>
+
+                    <div className="d-flex  align-items-center">
+                      <div className="d-flex flex-column align-items-center ">
+                        <img
+                          className="second_image"
+                          src={slide?.second_image}
+                          alt=""
+                          width={"30px"}
+                        />
+                        <small>{slide?.second_team}</small>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Link>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      }
-       
+                </Link>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
+
         <Col lg={12}>
           <Link
             to="/matches"
--- src/utils/axios-util.jsx
+++ src/utils/axios-util.jsx
@@ -2,6 +2,9 @@ import axios from "axios"
 import i18n from "../i18n"
 
  const baseURL = `https://elfinaly.com/api/v1/`
+ const baseURLEn = `https://v3.football.api-sports.io/`
+
+
  const url = "http://localhost:3000/"
 //  const url = "https://www.elmarma.com/"
 
@@ -24,10 +27,28 @@ import i18n from "../i18n"
     // "ngrok-skip-browser-warning": true
   },
 })
+const  apiClientEn=  axios.create({
+  baseURLEn,
+  headers: {
+    // Authorization: `Bearer ${token}`,
+    "Content-Type": `application/json`,
+    "x-rapidapi-host":"v3.football.api-sports.io",
+    "x-rapidapi-key":"135101a0a8e831d6f0e3c40095f167ab",
+    // "Content-Type": `multipart/form-data`,
+
+    'Accept': 'application/json',
+    // 'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    // "Accept-Language":lang,
+    // "ngrok-skip-browser-warning": true
+  },
+})
+
 
 export {
   lang , 
   apiClient,
   customLang,
+  apiClientEn,
   url
 }
